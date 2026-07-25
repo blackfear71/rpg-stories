9f896bf6da1da4afb649bfbf72f3beded41f372b
Pierre <pierre.hamonic71@gmail.com>
2026-07-25 16:27:55 +0200

    Effet de fumée

--- app/src/components/shared/Layout/Layout.jsx
+++ app/src/components/shared/Layout/Layout.jsx
@@ -15,6 +15,19 @@ const Layout = () => {
             {/* Barre de navigation */}
             {location.pathname !== '/' && <NavBar />}
 
+            {/* Effets de fumée */}
+            <div className="smoke smoke-left">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+            <div className="smoke smoke-right">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
             {/* Contenu */}
             <main className={`layout-main ${location.pathname !== '/' ? 'layout-main-with-navbar' : ''}`}>
                 <Outlet />
